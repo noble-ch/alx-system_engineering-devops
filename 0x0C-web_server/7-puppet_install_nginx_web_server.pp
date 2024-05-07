40d00d7f441e338cb63957e818586def29a51c11
noble <noblebarch@gmail.com>
2024-05-07 17:58:10 -0400

    comeback

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,43 +1,24 @@
-# configure nginx server to listen to port 80
-# with permanent redirect for /redirect_me and handle
-# custom error 404 page
+# Setup New Ubuntu server with nginx
 
-exec {'apt-get-update':
-  command => '/usr/bin/apt-get update'
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
 
-package {'nginx':
-  ensure  => 'installed',
-  require => Exec['apt-get-update'],
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
 file {'/var/www/html/index.html':
-  ensure  => 'present',
-  content => 'Holberton School',
-  require => Package['nginx']
+	content => 'Hello World!'
 }
 
-file_line {'server_name _;':
-  path    => '/etc/nginx/sites-available/default',
-  line    => "\n\tlocation /redirect_me {\n\t\trewrite ^/redirect_me(.*)$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n\t}",
-  after   => 'server_name _;',
-  require => Package['nginx']
-}
-
-file {'/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page",
-  require => Package['nginx']
-}
-
-file_line {'server_name _; ':
-  path    => '/etc/nginx/sites-enabled/default',
-  line    => "\n\terror_page 404 /404.html;\n\tlocation = /404.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}",
-  after   => 'server_name _;',
-  require => Package['nginx']
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
 service {'nginx':
-  ensure  => 'running',
-  require => Package['nginx']
+	ensure => running,
+	require => Package['nginx']
 }
